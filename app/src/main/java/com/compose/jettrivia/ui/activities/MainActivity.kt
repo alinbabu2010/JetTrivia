4bea81e90a4b81baf6002c3ff6f88024e96ec9a6
Alin Babu <alinbabu2010@gmail.com>
2022-05-06 12:35:35 +0530

    Modify MainActivity.kt to test implementations

--- app/src/main/java/com/compose/jettrivia/ui/activities/MainActivity.kt
+++ app/src/main/java/com/compose/jettrivia/ui/activities/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.compose.jettrivia.ui.activities
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,14 +10,18 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.compose.jettrivia.ui.theme.JetTriviaTheme
+import com.compose.jettrivia.ui.viewmodels.QuestionViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetTriviaTheme {
-                Content()
+                TriviaHome()
             }
         }
     }
@@ -24,20 +29,26 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun Content() {
+fun TriviaHome(viewModel: QuestionViewModel = hiltViewModel()) {
     // A surface container using the 'background' color from the theme
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colors.background
     ) {
-
+        Questions(viewModel)
     }
+}
+
+@Composable
+fun Questions(viewModel: QuestionViewModel) {
+    val questions = viewModel.data.value.data?.toMutableList()
+    Log.d("TAG", "Questions: ${questions?.size}")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetTriviaTheme {
-        Content()
+        TriviaHome()
     }
 }
