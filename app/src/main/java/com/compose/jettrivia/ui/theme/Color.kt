fffe657188e112ce5cc5bcdfdf02c5f8d2bd2d5b
Alin Babu <alinbabu2010@gmail.com>
2022-05-09 09:05:06 +0530

    Perform code inspection and remove unused resources

--- app/src/main/java/com/compose/jettrivia/ui/theme/Color.kt
+++ app/src/main/java/com/compose/jettrivia/ui/theme/Color.kt
@@ -3,13 +3,11 @@ package com.compose.jettrivia.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val LightGray = Color(0xffb4b9cd)
-val Black = Color(0xff060814)
 val OffWhite = Color(0xffeceeef)
 val LightPurple = Color(0xff545a75)
 val DarkPurple = Color(0xff262C49)
 val OffDarkPurple = Color(0xFF22496B)
 val LightBlue = Color(0xff127EEA)
-val Brown = Color(0xff7f6749)
 val Blue = Color(0xff4b5f9e)
 val OffPink = Color(0xfff95075)
 val OffPurple = Color(0xffbe6be5)
